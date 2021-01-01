ffd21acb1b88644069cfd9f5b8fb245188397ba3
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-01 14:23:09 +0100

    Rafactor MainView

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -5,9 +5,24 @@ const StyledMainView = styled.main`
    padding: 36px 55px;
    flex-grow: 1;
 
-   h1 {
+   > div {
+      margin: 0 auto;
+      max-width: 800px;
+   }
+
+   > div h1 {
       font-size: 20px;
       flex-grow: 1;
+      margin-bottom: 12px;
+   }
+
+   .icon-btn {
+      color: grey;
+   }
+
+   .icon-btn:hover {
+      color: #202020;
+      background-color: #eee;
    }
 `
 
