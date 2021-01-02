9ae7977f4be01befe9e328d994dccc7c793e4754
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-02 13:42:00 +0100

    Add popover component

---
+++ src/components/shared/Popover.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components/macro';
+
+const StyledPopover = styled.div`
+   > *:last-child {
+      position: absolute;
+      z-index: 10000;
+      background-color: red;
+      bottom: -50px;
+      opacity: ${props => props.isOpen ? '1' : '0'};
+      visibility: ${props => props.isOpen ? 'visible' : 'hidden'};
+      transition: all 0.2s;
+   }
+`
+
+function Popover({ activator, content }) {
+   const [open, toggle] = useState(false);
+   return (
+      <StyledPopover isOpen={open} onClick={() => toggle(!open)}>
+         {activator}
+         {content}
+      </StyledPopover>
+   )
+}
+
+Popover.pooptypes = {
+   activator: PropTypes.node.isRequired,
+   content: PropTypes.node.isRequired,
+}
+
+export default Popover
