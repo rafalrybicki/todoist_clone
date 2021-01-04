5803c6517c6e57b2b1109a4a8ac13404981a677e
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-04 13:39:01 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import styled from 'styled-components/macro';
 
 import AppBar from './components/AppBar';
@@ -7,6 +13,7 @@ import MainView from './views/MainView';
 import Inbox from './views/Inbox';
 import Today from './views/Today';
 import Upcoming from './views/Upcoming';
+import Project from './views/Project';
 
 import NewProject from './components/NewProject';
 import TaskEditor from './components/TaskEditor';
@@ -28,18 +35,24 @@ function App() {
   }
 
   return (
-    <StyledApp className="App">
-      <AppBar toggleMenu={toggleMenu} />
-      <AppMenu isOpen={menuOpen} close={toggleMenu}/>
-      <MainView>
-        {/* <Upcoming /> */}
-        <Inbox />
-        {/* <Today /> */}
-      </MainView>
-      {/* <NewProject /> */}
-      {/* <TaskEditor headerText="Quick Add Task"/> */}
-      {/* <TaskModal /> */}
-    </StyledApp>
+    <Router>
+      <StyledApp className="App">
+        <AppBar toggleMenu={toggleMenu} />
+        <AppMenu isOpen={menuOpen} close={toggleMenu}/>
+        <MainView>
+          <Switch>
+            <Route path="/inbox" component={Inbox} />
+            <Route path="/today" component={Today} />
+            <Route path="/upcoming" component={Upcoming} />
+            <Route path="/project/:id" component={Project} />
+            <Route path='/' render={() => <Redirect to= "/inbox" />} />
+          </Switch>
+        </MainView>
+        {/* <NewProject /> */}
+        {/* <TaskEditor headerText="Quick Add Task"/> */}
+        {/* <TaskModal /> */}
+      </StyledApp>
+    </Router>
   );
 }
 
