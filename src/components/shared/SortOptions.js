500ee7614835128a74fac6a237632b2b54b5b731
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-05 20:33:29 +0100

    Refactor SortOptions

--- src/components/shared/SortOptions.js
+++ src/components/shared/SortOptions.js
@@ -1,33 +1,33 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import MenuList from './MenuList';
 import { CalendarEvent, Flag, Gear, Person, SortAlphaDown } from 'react-bootstrap-icons';
 
-function SortOptions() {
+function SortOptions({ sortBy }) {
    return (
       <MenuList>
-         <li>
-            <CalendarEvent size={16} />
-            Sort by due date
+         <li className={sortBy === "date" ? "active" : ""}>
+            <CalendarEvent size={16} /> Sort by due date
          </li>
-         <li className="active">
-            <Flag size={16} />
-            Sort by priority
+         <li className={sortBy === "priority" ? "active" : ""}>
+            <Flag size={16} /> Sort by priority
          </li>
-         <li>
-            <SortAlphaDown size={16} />
-            Sort alphabetically
+         <li className={sortBy === "alphabetically" ? "active" : ""}>
+            <SortAlphaDown size={16} />Sort alphabetically
          </li>
-         <li>
-            <Person size={16} />
-            Sort by assignee
+         <li className={sortBy === "assignee" ? "active" : ""}>
+            <Person size={16} /> Sort by assignee
          </li>
-         <li>
-            <Gear size={16} />
-            Custom sort
+         <li className={sortBy === "custom" ? "active" : ""}>
+            <Gear size={16} /> Custom sortBy
          </li>
       </MenuList>
    )
+}
+
+SortOptions.propTypes = {
+   sortBy: PropTypes.string.isRequired
 }
 
 export default SortOptions
