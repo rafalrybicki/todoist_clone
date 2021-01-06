dd6315e9de3be859d746a40509c4f1fe3a52fac3
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-06 16:52:55 +0100

    Add onClick and PropTypes to CloseBtn

--- src/components/shared/buttons/CloseBtn.js
+++ src/components/shared/buttons/CloseBtn.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import IconBtn from './IconBtn';
 import { X } from 'react-bootstrap-icons';
 
-function CloseBtn() {
+function CloseBtn({ onClick }) {
    return (
-      <IconBtn width="26px" cssClass="close-btn">
+      <IconBtn 
+         width="26px"
+         onClick={onClick}
+         cssClass="close-btn"
+      >
          <X size={26} color="grey" />
       </IconBtn>
    )
+}
+
+CloseBtn.propTypes = {
+   onClick: PropTypes.func.isRequired
 }
 
 export default CloseBtn
