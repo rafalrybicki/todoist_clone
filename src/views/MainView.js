da0c4d080cc785600d3d3c4c916ee737d025ad93
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-09 14:27:08 +0100

    refactor MainView styles

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -2,49 +2,51 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 const StyledMainView = styled.main`
-   padding: 0 55px 36px;
    flex-grow: 1;
-   overflow-y: scroll;
-   overflow-x: hidden;
+   max-height: 100%;
+   overflow: hidden;
    z-index: 10;
 
    > div {
-      margin: 0 auto;
-      max-width: 800px;
-   }
+      position: relative;
+      width: 100%;
+      height: 100%;
+      padding: 36px 55px 0;
+      overflow-y: auto;
 
-   header {
-      display: flex;
-      position: sticky;
-      top: 0;
-      padding: 36px 55px 10px;
-      margin-left: -55px;
-      margin-right: -55px;
-      background-color: white;
-      z-index: 100;
-
-      h1 {
-         font-size: 20px;
-         flex-grow: 1;
-         display: flex;
-         align-items: center;
-         font-family: 'Roboto', sans-serif;
+      > * {
+         max-width: 800px;
+         margin: 0 auto;
       }
-   }
 
-   .icon-btn {
-      color: grey;
-   }
+      header {
+         display: flex;
+         position: sticky;
+         background-color: white;
+         z-index: 100;
 
-   .icon-btn:hover {
-      color: #202020;
-      background-color: #eee;
+         h1 {
+            font-size: 20px;
+            flex-grow: 1;
+            display: flex;
+            align-items: center;
+         }
+      }
+
+      .icon-btn {
+         color: grey;
+      }
+
+      .icon-btn:hover {
+         color: #202020;
+         background-color: #eee;
+      }
    }
 `
 
 function MainView({ children }) {
    return (
-      <StyledMainView>
+      <StyledMainView id="main">
          {children}
       </StyledMainView>
    )
