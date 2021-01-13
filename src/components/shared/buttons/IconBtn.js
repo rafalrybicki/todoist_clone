0402b3ace64bfb776901b180a4b6e2bf01f0f5cc
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-13 18:53:45 +0100

    Change IconBtn

--- src/components/shared/buttons/IconBtn.js
+++ src/components/shared/buttons/IconBtn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components/macro';
 
 const StyledIconBtn = styled.button`
@@ -10,46 +11,57 @@ const StyledIconBtn = styled.button`
    height: ${props => props.height};
 
    @media screen and (min-width: 750px) {
-      &::after {
+      span {
          position: absolute;
          z-index: 1001;
          bottom: -31px;
-         content: "${props => props.tooltip}";
          color: white;
-         display: none;
-         background-color: rgba(0,0,0,.7);
-         padding: 3px 0 1px;
+         display: block;
          width: ${props => props.tooltipWidth};
+         background-color: rgba(0,0,0,.7);
+         padding-top: 1.5px;
          height: 20px;
          border-radius: 3px;
          font-size: 12px;
          text-align: center;
+         opacity: 0;
+         transition: opacity .1s
       }
 
       &:hover {
          background-color: ${props => props.hoverColor};
 
-         &::after {
-            display: block
+         span {
+            opacity: 1;
          }
       }
    }
 `
 
-function IconBtn({ width='32px', height, onClick, tooltip, tooltipWidth, cssClass, children }) {
+function IconBtn({ width='32px', height, onClick, tooltip, tooltipWidth, className, children }) {
    return (
       <StyledIconBtn
          width={width}
          height={height || width}
+         tooltipWidth={tooltipWidth}
          onClick={onClick ? onClick : undefined }
          type="button"
-         tooltip={tooltip}
-         tooltipWidth={tooltipWidth}
-         className={cssClass ? "icon-btn " + cssClass : "icon-btn"}
+         className={className ? "icon-btn " + className : "icon-btn"}
       >
          {children}
+         {tooltip && <span>{tooltip}</span>}
       </StyledIconBtn>
    )
+}
+
+IconBtn.propTypes = {
+   width: PropTypes.string,
+   height: PropTypes.string,
+   onClick: PropTypes.func,
+   tooltip: PropTypes.string,
+   tooltipWidth: PropTypes.string,
+   className: PropTypes.string,
+   children: PropTypes.node.isRequired,
 }
 
 export default IconBtn
