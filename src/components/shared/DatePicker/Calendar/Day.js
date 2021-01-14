12220ecf325983ac33a878ed2c25d17b7583b32d
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-14 18:00:40 +0100

    Add CalendarDay

---
+++ src/components/shared/DatePicker/Calendar/Day.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components/macro';
+
+const StyledCalendarDay = styled.td`
+   button {
+      width: 24px;
+      height: 24px;
+      border-radius: 50%;
+      font-size: 13px;
+      color: ${props => props.active? 'white' : '#202020'};
+      background-color: ${props => props.active? 'red' : 'transparent'};
+      padding-right: ${props => props.number > 9 ? '1px' : '0'};
+
+      &:disabled {
+         opacity: .1
+      }
+
+      &:hover {
+         background-color: #eee;
+         color: black;
+      }
+   }
+`
+
+function CalendarDay({ number, active, onClick, disabled }) {
+   return (
+      <StyledCalendarDay
+         active={active}
+         number={number}
+      >
+         <button 
+            type="button"
+            onClick={onClick}
+            disabled={disabled}
+         >
+            {number}
+         </button>
+      </StyledCalendarDay>
+   )
+}
+
+CalendarDay.propTypes = {
+   number: PropTypes.number.isRequired,
+   active: PropTypes.bool.isRequired,
+   onClick: PropTypes.func.isRequired,
+   disabled: PropTypes.bool.isRequired
+}
+
+export default CalendarDay
