66d78d7ff39210b237733daf2cc4e1af0112d99c
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-20 16:05:19 +0100

    Change Login and Signup

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,124 +1,14 @@
 import React from 'react';
-import styled from 'styled-components/macro';
 
 import { auth } from '../firebase';
+import { Redirect } from 'react-router-dom';
+import EntranceView from './EntranceView';
 import { Link } from 'react-router-dom';
 
-const StyledLogin = styled.div`
-   position: relative;
-   width: 100vw;
-   height: 100vh;
-   background-color: white;
-   padding: 10px;
-   margin-top: -40px;
-   font-size: 13px;
-   transition: height .05s;
-
-   h1 {
-      margin: 8px 0 27px;
-      font-size: 26px;
-   }
-
-   button {
-      height: 38px;
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border: solid 1px #ddd;
-      border-radius: 3px;
-      margin-bottom: 10px;
-      transition: all .2s;
-
-      &[type="submit"] {
-         background-color: red;
-         color: white;
-         font-weight: bold;
-         margin: 15px 0;
-      }
-
-      &:hover {
-         border-color: #202020;
-         background-color: #eee;
-      }
-
-      img {
-         max-height: 16px;
-         max-width: 16px;
-         margin-right: 8px;
-      }
-   }
-
-   .separator {
-      position: relative;
-      z-index: 10;
-      display: block;
-      width: 40px;
-      text-align: center;
-      background-color: white;
-      margin: 20px auto 10px;
-      color: #777777;
-   }
-
-   &:after {
-      content: '';
-      position: absolute;
-      top: 174px;
-      left: 10px;
-      height: 1px;
-      width: calc(100% - 20px);
-      background-color: #ddd;
-   }
-
-   label {
-      display: block;
-      font-weight: bold;
-      margin-bottom: 10px;
-      margin-top: 15px;
-      
-      &.checkbox-label {
-         font-weight: normal;
-         position: relative;
-         top: -1px;
-         display: inline-block;
-         margin: 0 0 15px 5px;
-      }
-   }
-
-   input[type="email"], input[type="password"] {
-      width: 100%;
-      height: 38px;
-      border: solid 1px #ddd;
-      border-radius: 3px;
-   }
-
-   > p {
-      border-top: 1px solid #eee;
-      padding-top: 20px;
-      text-align: center;
-
-      a {
-         color: red;
-      }
-   }
-   
-   @media (min-width: 450px) {
-      max-width: 452px;
-      height: fit-content;
-      margin: 0 auto;
-      padding: 25px;
-      border: solid 1px #dbdbdb;
-      border-radius: 8px;
-
-      &:after {
-         top: 189px;
-         left: 25px;
-         width: 402px;
-      }
-   }
-`
-
 function Login({ history }) {
+   if (auth.currentUser && auth.currentUser.uid) {
+      return <Redirect to="/index" />
+   }
 
    function loginWithGoogle() {
       alert('coming soon')
@@ -128,14 +18,12 @@ function Login({ history }) {
       e.preventDefault();
 
       auth.signInWithEmailAndPassword(e.target.email.value, e.target.password.value)
-         .then(() => history.push('/inbox'))
-         .catch((error) => {
-            alert(error.message)
-         });
+         .then(() => history.push("/index"))
+         .catch(error => alert(error.message));
    }
 
    return (
-      <StyledLogin>
+      <EntranceView>
          <a href="/">
             <img src="https://d3ptyyxy2at9ui.cloudfront.net/logo-e7e40b.svg" alt="Todoist logo"/>
          </a>
@@ -157,7 +45,7 @@ function Login({ history }) {
                <label htmlFor="permanent_login" className="checkbox-label">Keep me logged in</label>
          </form>
          <p> Don't have an account? <Link to="/signup">Sign up</Link></p>
-      </StyledLogin>
+      </EntranceView>
    )
 }
 
--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -1,126 +1,15 @@
 import React from 'react';
-import styled from 'styled-components/macro';
 
 import { auth, db } from '../firebase';
-
+import { Redirect } from 'react-router-dom';
+import EntranceView from './EntranceView';
 import { Link } from 'react-router-dom';
 
-const StyledSignIn = styled.div`
-   position: relative;
-   width: 100vw;
-   height: 100vh;
-   background-color: white;
-   padding: 10px;
-   margin-top: -40px;
-   font-size: 13px;
-   transition: height .05s;
-   overflow-y: auto;
-
-   h1 {
-      margin: 8px 0 27px;
-      font-size: 26px;
+function SignUp({ history }) {
+   if (auth.currentUser && auth.currentUser.uid) {
+      return <Redirect to="/index" />
    }
 
-   button {
-      height: 38px;
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border: solid 1px #ddd;
-      border-radius: 3px;
-      margin-bottom: 10px;
-      transition: all .2s;
-
-      &[type="submit"] {
-         background-color: red;
-         color: white;
-         font-weight: bold;
-         margin: 15px 0;
-      }
-
-      &:hover {
-         border-color: #202020;
-         background-color: #eee;
-      }
-
-      img {
-         max-height: 16px;
-         max-width: 16px;
-         margin-right: 8px;
-      }
-   }
-
-   .separator {
-      position: relative;
-      z-index: 10;
-      display: block;
-      width: 40px;
-      text-align: center;
-      background-color: white;
-      margin: 20px auto 10px;
-      color: #777777;
-   }
-
-   &:after {
-      content: '';
-      position: absolute;
-      top: 174px;
-      left: 10px;
-      height: 1px;
-      width: calc(100% - 20px);
-      background-color: #ddd;
-   }
-
-   label {
-      display: block;
-      font-weight: bold;
-      margin-bottom: 10px;
-      margin-top: 15px;
-      
-      &.checkbox-label {
-         font-weight: normal;
-         position: relative;
-         top: -1px;
-         display: inline-block;
-         margin: 0 0 15px 5px;
-      }
-   }
-
-   input[type="text"], input[type="email"], input[type="password"] {
-      width: 100%;
-      height: 38px;
-      border: solid 1px #ddd;
-      border-radius: 3px;
-   }
-
-   > p {
-      border-top: 1px solid #eee;
-      padding-top: 20px;
-      text-align: center;
-
-      a {
-         color: red;
-      }
-   }
-   
-   @media (min-width: 450px) {
-      max-width: 452px;
-      height: fit-content;
-      margin: 0 auto;
-      padding: 25px;
-      border: solid 1px #dbdbdb;
-      border-radius: 8px;
-
-      &:after {
-         top: 189px;
-         left: 25px;
-         width: 402px;
-      }
-   }
-`
-
-function Login({ history }) {
    function signupWithGoogle() {
       alert('coming soon')
    }
@@ -130,21 +19,19 @@ function Login({ history }) {
       const { name, email, password } = e.target;
 
       auth.createUserWithEmailAndPassword(email.value, password.value)
-         .then((res) => {
-            const path = 'users/' + res.user.uid;
-            db.ref(path).set({
+         .then(res => {
+            db.collection('users').doc(res.user.uid).set({
                id: res.user.uid,
-               name: name.value,
+               displayName: name.value,
                email: email.value,
             });
          })
-         .then(() => history.push('/inbox'))
-         .catch((error) => {
-            alert(error.message)
-         })
+         .then(() => history.push("/index"))
+         .catch(error => alert(error.message))
    }
+
    return (
-      <StyledSignIn>
+      <EntranceView>
          <a href="/">
             <img src="https://d3ptyyxy2at9ui.cloudfront.net/logo-e7e40b.svg" alt="Todoist logo"/>
          </a>
@@ -169,8 +56,8 @@ function Login({ history }) {
             <label htmlFor="permanent_login" className="checkbox-label">Keep me logged in</label>
          </form>
          <p>Already signed up? <Link to="/login">Go to login</Link></p>
-      </StyledSignIn>
+      </EntranceView>
    )
 }
 
-export default Login
+export default SignUp
