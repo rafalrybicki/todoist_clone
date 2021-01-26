4521bc7f6744407eed9734739051a6ac6b79a2a1
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-26 14:12:45 +0100

    Refactor ProjectSection

--- src/components/common/ProjectSection.js
+++ src/components/common/ProjectSection.js
@@ -1,124 +1,107 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components/macro';
 
+import Editor from '../Editor';
 import Grip from './Grip';
 import IconBtn from './buttons/IconBtn';
 import ChevronIcon from './icons/ChevronIcon';
 import { ThreeDots } from 'react-bootstrap-icons';
 import Task from '../Task';
-import NewItemBtn from './buttons/NewItemBtn';
-import Editor from '../Editor';
+import NewTask from './NewTask';
 
-import { db } from '../../firebase';
-import useFirestoreCollection from '../../hooks/useFirestoreCollection'
-
-const StyledProjectSection = styled.div`
+const StyledProjectSection = styled.section`
    position: relative;
-   padding: 20px 0 10px;
    width: 100%;
+   padding-bottom: 20px;
 
-   h2{
-      font-size: 14px;
-      margin: 0 5px 5px 0;
-      padding-bottom: 6px;
+   header {
+      display: flex;
       border-bottom: 1px solid #f0f0f0;
-   }
 
-   /* > ul {
-      margin-top: 5px;
-      padding-top: 5px;
-      border-top: 1px solid #f0f0f0;
-      width: 100%;
-      height: ${props => props.showList ? 'auto' : '0'};
-      transition: height .2s;
-      overflow: hidden;
-   } */
+      .grip {
+         top: -2px;
+         left: -60px;
 
-   > .grip {
-      top: 18px;
-      left: -60px;
-   }
-
-   > .chevron {
-      position: absolute !important;
-      top: 18px;
-      left: -30px;
-
-      svg {
-         color: grey;
+         &:hover {
+            background-color: #eee;
+         }
       }
-   }
+      
+      .chevron {
+         position: absolute;
+         top: -2px;
+         left: -30px;
 
-   > .chevron:hover {
-      svg {
-         color: #202020
+         svg {
+            color: grey;
+         }
+
+         &:hover {
+            svg {
+               color: #202020
+            }
+         }
       }
-   }
 
-   &:hover {
-      > .grip {
-         color: grey;
+      h2{
+         font-size: 14px;
+         margin: 0 5px 5px 0;
+         flex-grow: 1;
       }
-   }
 
-   .more {
-      position: absolute;
-      top: 18px;
-      right: 0
+      &:hover {
+         .grip {
+            color: grey;
+         }
+      }
    }
 `
 
-function ProjectSection({ name, sectionId, projectId, userId }) {
-   const tasks = useFirestoreCollection('tasks', 'sectionId', '==', sectionId);
-   const [showList, toggleSectionList] = useState(true);
-   const [editor, toggleEditor] = useState(false);
+function ProjectSection({ name, sectionId, projectId, isOpen }) {
+   const tasks = useSelector(state => state.tasks.filter(task => task.projectId === projectId && task.section.id === sectionId));
+   const [openEditor, setOpenEditor] = useState(false);
 
-   const addNewTask = (obj) => {
-      let newTask = {
-         projectId,
-         sectionId,
-         ownerId: userId,
-         priority: 4,
-         targetDate: null,
-         completionDate: null,
-         subTasks: [],
-         comments: [],
-         activity: []
-      }
+   const editSection = (newName) => alert('change name from ' + name + ' to ' + newName) 
 
-      const newTaskRef = db.collection('tasks').doc();
-      const id = newTaskRef.id
-
-      newTask = {
-         id,
-         ...newTask,
-         ...obj
-      }
-      
-      newTaskRef.set(newTask);
-   }
+   const toggleSectionOpening = () => alert('open section')
 
    return (
-      <StyledProjectSection showList={showList}>
-         <Grip />
-         <IconBtn
-            hoverColor="#eee"
-            width="28px"
-            onClick={() => toggleSectionList(!showList)}
-            className="chevron"
-         >
-            <ChevronIcon rotate={showList.toString()} />
-         </IconBtn>
-         <h2>{name}</h2>
-         <IconBtn
-            hoverColor="#eee"
-            width="28px"
-            tooltip="More section actions"
-            tooltipWidth="112px"
-            className="more"
-         >
-            <ThreeDots size="20"/>
-         </IconBtn>
+      <StyledProjectSection isOpen={isOpen}>
+         {openEditor &&
+            <Editor
+               currentContent={name}
+               onSave={editSection}
+               onClose={() => setOpenEditor(false)}
+            />
+         }
+
+         {sectionId !== 'default' && openEditor === false &&
+            <header>
+               <Grip />
+               <IconBtn
+                  hoverColor="#eee"
+                  width="28px"
+                  onClick={toggleSectionOpening}
+                  className="chevron"
+               >
+                  <ChevronIcon rotate={isOpen} />
+               </IconBtn>
+               <h2>{name}</h2>
+               <IconBtn
+                  hoverColor="#eee"
+                  width="28px"
+                  tooltip="More section actions"
+                  tooltipWidth="112px"
+                  onClick={() => setOpenEditor(true)}
+                  className="more"
+               >
+                  <ThreeDots size="20"/>
+               </IconBtn>
+            </header>
+         }
+
          {tasks.map( task => 
             <Task
                key={task.id}
@@ -128,27 +111,25 @@ function ProjectSection({ name, sectionId, projectId, userId }) {
                content={task.content}
                priority={task.priority}
                targetDate={task.targetDate}
+               targetDateTime={task.targetDateTime}
                completionDate={task.completionDate}
                subTasks={task.subTasks}
             />
          )}
-         {!editor && 
-            <NewItemBtn
-               text="Add task"
-               onClick={() => toggleEditor(true)}
-            />
-         }
-         {editor &&
-            <Editor
-               currentProjectId={projectId}
-               currentSectionId={sectionId}
-               onSave={addNewTask}
-               onClose={() => toggleEditor(false)}
-               isTask
-            />
-         }
+
+         <NewTask
+            sectionId={sectionId}
+            projectId={projectId}
+         />
       </StyledProjectSection>
    )
+}
+
+ProjectSection.propTypes = {
+   name: PropTypes.string.isRequired,
+   sectionId: PropTypes.string.isRequired,
+   projectId: PropTypes.string.isRequired,
+   isOpen: PropTypes.bool.isRequired
 }
 
 export default ProjectSection
