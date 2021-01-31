2fe344496c864f9233b6ff0917984f2a53e682ef
Rafal Rybicki <rafarybicki@gmail.com>
2021-01-31 13:09:02 +0100

    Change Calendar

--- src/components/common/selectors/Calendar/index.js
+++ src/components/common/selectors/Calendar/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/macro';
 
-import { getDate, getMonth, getFirstDayOfTheMonth, getLastDayOfTheMonth, getBeginingOfTheDay } from '../../../../utils'
+import { getDate, getMonth, getFirstDayOfTheMonth, getLastDayOfTheMonth } from '../../../../utils'
 import Header from './Header';
 import Month from './Month';
 import Day from './Day'
@@ -12,9 +12,8 @@ const StyledCalendar = styled.div`
 `
 
 function Calendar({ currentDate, setDate }) {
-   const currentDateStart = getBeginingOfTheDay(currentDate);
-   const date = currentDate ? getDate(currentDateStart) : getDate();
    const today = getDate();
+   const date = currentDate ? getDate(currentDate) : today;
    const miliseconds = date.miliseconds;
    const [year, setYear] = useState(date.year);
    const [month, setMonth] = useState(date.month);
@@ -24,8 +23,6 @@ function Calendar({ currentDate, setDate }) {
    const monthArr = getMonth(month, year);
 
    const reset = () => {
-      setDate(null);
-      const today = getDate()
       setYear(today.year)
       setMonth(today.month)
    }
@@ -50,7 +47,7 @@ function Calendar({ currentDate, setDate }) {
                      <Day
                         key={day.miliseconds}
                         number={day.day}
-                        active={day.miliseconds === miliseconds && currentDate > today.miliseconds}
+                        active={day.miliseconds === miliseconds && currentDate >= today.miliseconds}
                         disabled={day.miliseconds < firstDay || day.miliseconds > lastDay || day.miliseconds < today.miliseconds}
                         onClick={() => setDate(day.miliseconds)}
                      />
